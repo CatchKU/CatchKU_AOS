85864022b74b2adc9083f47e155fd1a3c733714c
t1nm1ksun <minsuk07672@naver.com>
2024-05-14 13:44:43 +0900

    .

--- app/src/main/java/com/example/catchku/screen/MapScreen.kt
+++ app/src/main/java/com/example/catchku/screen/MapScreen.kt
@@ -1,12 +1,11 @@
 package com.example.catchku.screen
 
-import android.graphics.drawable.BitmapDrawable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.materialIcon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import com.example.catchku.R
 import com.naver.maps.geometry.LatLng
@@ -21,16 +20,21 @@ import com.naver.maps.map.compose.rememberCameraPositionState
 import com.naver.maps.map.compose.rememberFusedLocationSource
 import com.naver.maps.map.overlay.OverlayImage
 
+
 @OptIn(ExperimentalNaverMapApi::class)
 @Composable
 fun MapScreen(navController: NavHostController) {
+    val context = LocalContext.current
+
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         val cameraPositionState = rememberCameraPositionState()
         NaverMap(
+
             modifier = Modifier.fillMaxSize(),
+
             cameraPositionState = cameraPositionState,
             locationSource = rememberFusedLocationSource(),
             properties = MapProperties(
@@ -40,49 +44,7 @@ fun MapScreen(navController: NavHostController) {
                 isLocationButtonEnabled = true,
             ),
         ) {
-            //행정관
-            SetMarker(latitude = 37.5431505, longitude = 127.0751552)
-            //경영관
-            SetMarker(latitude = 37.5442615, longitude = 127.0760717)
-            //상허연구관
-            SetMarker(latitude = 37.5441682, longitude = 127.0753535)
-            //교육과학관
-            SetMarker(latitude = 37.5439837, longitude = 127.0742108)
-            //예술문화관
-            SetMarker(latitude = 37.542845, longitude = 127.0729332)
-            //언어교육원
-            SetMarker(latitude = 37.5426356, longitude = 127.074649)
-            //박물관
-            SetMarker(latitude = 37.5423945, longitude = 127.0756472)
-            //법학관
-            SetMarker(latitude = 37.5419017, longitude = 127.0749445)
-            //상허기념도서관
-            SetMarker(latitude = 37.5419226, longitude = 127.0737408)
-            //의생명과학연구관
-            SetMarker(latitude = 37.5415596, longitude = 127.0721872)
-            //생명과학관
-            SetMarker(latitude = 37.5407426, longitude = 127.0735979)
-            //동물생명과학관
-            //입학정보관
-            //산학협동관
-            //수의학관
-            //새천년관
-            //건축관
-            //해봉부동산학과
-            //인문학관
-            //학생회관
-            //공학관
-            SetMarker(latitude = 37.54165947462257, longitude = 127.07881987094879)
-            //신공학관
-            SetMarker(latitude = 37.54054930197007, longitude = 127.0794689655304)
-            //과학관
-            //창의관
-            //KU기술혁신관
-            //쿨하우스
-            //건대부중
-            //건대부고
-            //건국대학교병원
-            SetMarker(latitude = 37.54060, longitude = 127.0717)
+            DrawMarker()
         }
     }
 }
@@ -101,12 +63,72 @@ fun SetMarker(latitude: Double, longitude: Double) {
     }
 }
 
+@OptIn(ExperimentalNaverMapApi::class)
+@Composable
+fun SetUserMarker(latitude: Double, longitude: Double) {
+
+    Marker(
+        //TODO: 사용자 위치 받아오기
+        state = MarkerState(position = LatLng(latitude, longitude)),
+        icon = OverlayImage.fromResource(R.drawable.ku),
+        onClick = {}
+    )
+}
+
+
+
 fun isVisible(): Boolean {
-    //다른 유저가 3분이내에 해당 쿠에 접근했나?
+    //다른 유저가 3분 이내에 해당 쿠에 접근했나?
     if (true) {
         return true
-    }
-    else {
+    } else {
         return false
     }
+}
+
+@Composable
+fun DrawMarker(){
+    //행정관
+    SetMarker(latitude = 37.5431505, longitude = 127.0751552)
+    //경영관
+    SetMarker(latitude = 37.5442615, longitude = 127.0760717)
+    //상허연구관
+    SetMarker(latitude = 37.5441682, longitude = 127.0753535)
+    //교육과학관
+    SetMarker(latitude = 37.5439837, longitude = 127.0742108)
+    //예술문화관
+    SetMarker(latitude = 37.542845, longitude = 127.0729332)
+    //언어교육원
+    SetMarker(latitude = 37.5426356, longitude = 127.074649)
+    //박물관
+    SetMarker(latitude = 37.5423945, longitude = 127.0756472)
+    //법학관
+    SetMarker(latitude = 37.5419017, longitude = 127.0749445)
+    //상허기념도서관
+    SetMarker(latitude = 37.5419226, longitude = 127.0737408)
+    //의생명과학연구관
+    SetMarker(latitude = 37.5415596, longitude = 127.0721872)
+    //생명과학관
+    SetMarker(latitude = 37.5407426, longitude = 127.0735979)
+    //동물생명과학관
+    //입학정보관
+    //산학협동관
+    //수의학관
+    //새천년관
+    //건축관
+    //해봉부동산학과
+    //인문학관
+    //학생회관
+    //공학관
+    SetMarker(latitude = 37.54165947462257, longitude = 127.07881987094879)
+    //신공학관
+    SetMarker(latitude = 37.54054930197007, longitude = 127.0794689655304)
+    //과학관
+    //창의관
+    //KU기술혁신관
+    //쿨하우스
+    //건대부중
+    //건대부고
+    //건국대학교병원
+    SetMarker(latitude = 37.54060, longitude = 127.0717)
 }
