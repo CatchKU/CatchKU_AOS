4151a82fbebe0931717fde8e65f0db4694871267
suyeun84 <kelsey6225@naver.com>
2024-06-01 21:30:46 +0900

    feat: UserRanking 서버 불러오기

--- app/src/main/java/com/example/catchku/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/catchku/data/repository/UserRepositoryImpl.kt
@@ -4,6 +4,7 @@ import com.example.catchku.data.model.request.RequestUserLoginDto
 import com.example.catchku.data.model.request.RequestUserRegisterDto
 import com.example.catchku.data.model.response.ResponseRegisterDto
 import com.example.catchku.data.model.response.ResponseTopFiveDepartmentDto
+import com.example.catchku.data.model.response.ResponseTopFiveUserDto
 import com.example.catchku.data.source.UserDataSource
 import com.example.catchku.domain.repository.UserRepository
 import javax.inject.Inject
@@ -14,6 +15,11 @@ class UserRepositoryImpl @Inject constructor(
     override suspend fun getTopFiveDepartment(): Result<ResponseTopFiveDepartmentDto> =
         runCatching {
             userDataSource.getKUTopFiveDepartment()
+        }
+
+    override suspend fun getTopFiveUser(): Result<ResponseTopFiveUserDto> =
+        runCatching {
+            userDataSource.getTopFiveUser()
         }
 
     override suspend fun postRegisterUser(requestUserRegisterDto: RequestUserRegisterDto): Result<ResponseRegisterDto> =
--- app/src/main/java/com/example/catchku/domain/repository/UserRepository.kt
+++ app/src/main/java/com/example/catchku/domain/repository/UserRepository.kt
@@ -4,9 +4,12 @@ import com.example.catchku.data.model.request.RequestUserLoginDto
 import com.example.catchku.data.model.request.RequestUserRegisterDto
 import com.example.catchku.data.model.response.ResponseRegisterDto
 import com.example.catchku.data.model.response.ResponseTopFiveDepartmentDto
+import com.example.catchku.data.model.response.ResponseTopFiveUserDto
 
 interface UserRepository {
     suspend fun getTopFiveDepartment(): Result<ResponseTopFiveDepartmentDto>
+
+    suspend fun getTopFiveUser(): Result<ResponseTopFiveUserDto>
 
     suspend fun postRegisterUser(requestUserRegisterDto: RequestUserRegisterDto): Result<ResponseRegisterDto>
 
