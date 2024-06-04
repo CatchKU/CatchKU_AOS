9b8493282a8912c42629bd7b09c6978bad37c75d
t1nm1ksun <122257945+t1nm1ksun@users.noreply.github.com>
2024-06-04 14:54:32 +0900

    Update ItemScreen.kt

--- app/src/main/java/com/example/catchku/screen/item/ItemScreen.kt
+++ app/src/main/java/com/example/catchku/screen/item/ItemScreen.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -148,17 +150,19 @@ fun ItemCard(item: ItemInfo, itemScreenViewModel: ItemScreenViewModel) {
                 color = Color(0xFFB5B6B7)
 
             )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "사용하기",
+                modifier = Modifier.clickable {
+                    Log.d("useItem","id: ${itemScreenViewModel.initUserId.value} itemName: ${item.itemName}")
+                    itemScreenViewModel.deleteUseItem(
+                        itemScreenViewModel.initUserId.value,
+                        item.itemName
+                    )
+                })
         }
 
-        Text(text = "사용하기",
-            modifier = Modifier.clickable {
-                Log.d("useItem","id: ${itemScreenViewModel.initUserId.value} itemName: ${item.itemName}")
-                itemScreenViewModel.deleteUseItem(
-                    itemScreenViewModel.initUserId.value,
-                    item.itemName
-                )
-            })
     }
+
 }
 
 @Composable
